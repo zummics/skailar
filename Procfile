3fb7c92b7e790daabbe10c37bc70e2566684521f
zummics <115847980+zummics@users.noreply.github.com>
2022-10-25 12:35:35 +0200

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn skailar.wsgi --log-file -
---
+++ skailar/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for skailar project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skailar.settings')
+
+application = get_wsgi_application()
